ea9b6bc7d8a7472e495cf27b1d7e12bea36fddcf
SanketPatel <sanketpatel.1805@gmail.com>
2017-02-23 23:50:14 -0800

    Stringy & Networky

---
+++ FSharp/Practice/Practice/Stringy.fsx
@@ -0,0 +1,24 @@
+﻿
+
+let showWordCount text =
+
+    let splitAtSpaces (text: string) =  
+        text.Split ' '
+        |> List.ofArray
+    
+    let wordCount text =
+        let words = splitAtSpaces text
+        let wc = words.Length
+        let distinctWords = List.distinct words
+        let dups = wc - distinctWords.Length
+        (wc, dups)
+
+    let showResults (wc, dups) =
+        printfn "--> %d words in the text" wc
+        printfn "--> %d duplicate words" dups
+
+    text
+    |> wordCount
+    |> showResults
+
+showWordCount "a aa c cc cc aa dd aa cc"

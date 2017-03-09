e4cb129b5c6f1b6b3d1bffb8421fac0c80b7218b
SanketPatel <sanketpatel.1805@gmail.com>
2017-03-09 07:54:11 -0800

    update to ActivePatterns

--- FSharp/Practice/Practice/ActivePatterns.fsx
+++ FSharp/Practice/Practice/ActivePatterns.fsx
@@ -12,10 +12,28 @@ module A =
         member x.RealPart = r
         member x.ImaginaryPart = i
 
+    fsi.AddPrinter (fun (c: Complex) -> sprintf "%gr + %gi" c.RealPart c.ImaginaryPart)
+
     let (|Rect|) (x: Complex) = (x.RealPart, x.ImaginaryPart)
     let (|Polar|) (x: Complex) = (x.Magnitude, x.Phase)
 
+    let add a b =
+        match a, b with
+        | Rect (ar, ai), Rect (br, bi) -> Complex (ar + br, ai + bi)
+
+    let multViaRect a b =
+        match a, b with
+        | Rect (ar, ai), Rect (br, bi) -> Complex (ar * br - ai * bi, ai * br + bi * ar)
+
+    let multViaPolar a b =
+        match a, b with
+        | Polar (m1, p1), Polar (m2, p2) -> Complex.Polar (m1 * m2, p1 + p2)
+
     let c = Complex (3.0, 4.0)
+
+    add c c
+    multViaRect c c
+    multViaPolar c c
 
     match c with
     | Rect (x, y) -> printfn "x = %g, y = %g" x y

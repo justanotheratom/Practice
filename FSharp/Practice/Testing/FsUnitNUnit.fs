43369de81dfeb7786c2529649ae8bba22a44f4ca
SanketPatel <sanketpatel.1805@gmail.com>
2017-03-25 09:48:49 -0700

    Adding a Testing project. And FsUnit.NUnit test basics

---
+++ FSharp/Practice/Testing/FsUnitNUnit.fs
@@ -0,0 +1,25 @@
+﻿module FsUnitNUnit
+
+// Double-backtick syntax.
+// Can have standalone test functions.
+// 'should equal ..' instead of 'Assert.AreEquals'.
+
+// ??: Creating parameterized tests is a pain. (Fuchu looks better).
+
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type TestClass () =
+
+    [<Test>]
+    member x.``Test in a class`` () =
+        2 + 2 |> should equal 4
+
+    [<Test>]
+    member x.``Test 2 in a class``() =
+        2 + 3 |> should equal 5
+
+[<Test>]
+let ``Standalone Test a Function`` () =
+    2 + 2 |> should equal 4

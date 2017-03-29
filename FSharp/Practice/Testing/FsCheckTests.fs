6529d17439ca9f07fd9b5cff9489cf574575c255
SanketPatel <sanketpatel.1805@gmail.com>
2017-03-28 20:46:23 -0700

    FsCheck simple test

---
+++ FSharp/Practice/Testing/FsCheckTests.fs
@@ -0,0 +1,15 @@
+﻿module FsCheckTests
+
+// Scott Wlaschin:
+// Property based testing is especially useful for "algorithmic" code
+// If you reverse a list and then reverse it again, you get the original list.
+// If you factorize an integer and then multiply the factors, you get the original number.
+
+open NUnit.Framework
+open FsCheck
+open FsUnit
+
+let revRevIsOrig (xs: list<int>) = List.rev (List.rev xs) = xs
+
+[<Test>]
+let listRevTest () = Check.Quick revRevIsOrig
